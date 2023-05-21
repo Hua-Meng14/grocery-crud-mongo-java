8b7d85de1140fb57ea7d71c1f7ca99c3414aad9b
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-05-21 09:48:38 +0700

    code base

---
+++ src/main/java/com/example/mongogroceriesspringboot/MongoGroceriesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongogroceriesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoGroceriesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoGroceriesSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/mongogroceriesspringboot/model/GroceryItem.java
@@ -0,0 +1,20 @@
+package com.example.mongogroceriesspringboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("groceryitems")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class GroceryItem {
+
+    @Id
+    private String id;
+    private
+}
