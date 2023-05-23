16c90d86b702f92fd0503456e119ab61c6123470
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-05-23 22:52:59 +0700

    add controller

--- src/main/java/com/example/mongogroceriesspringboot/controller/GroceryItemController.java
+++ src/main/java/com/example/mongogroceriesspringboot/controller/GroceryItemController.java
@@ -1,4 +1,41 @@
 package com.example.mongogroceriesspringboot.controller;
 
+
+import com.example.mongogroceriesspringboot.model.GroceryItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/items")
 public class GroceryItemController {
+
+    @Autowired
+    private GroceryItemService groceryItemService;
+
+    @PostMapping
+    public GroceryItem createGroceryItem(@RequestBody GroceryItem groceryItem) {
+        return groceryItemService.createGroceryItem(groceryItem);
+    }
+
+    @GetMapping
+    public List<GroceryItem> getAllGroceryItem() {
+        return groceryItemService.getAllGroceryItems();
+    }
+
+    @GetMapping("/{name}")
+    public GroceryItem getGroceryItemByName(@PathVariable String name) {
+        return groceryItemService.getGroceryItemByName(name);
+    }
+
+    @PutMapping("/{name}/category")
+    public  GroceryItem updateItemCategory(@PathVariable String name, @RequestParam String category) {
+        return groceryItemService.updateItemCategory(name, category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteGroceryItem(@PathVariable String id) {
+        groceryItemService.deleteGroceryItem(id);
+    }
 }
---
+++ src/main/java/com/example/mongogroceriesspringboot/service/GroceryItemService.java
@@ -0,0 +1,6 @@
+package com.example.mongogroceriesspringboot.service;
+
+public class GroceryItemService {
+
+
+}
